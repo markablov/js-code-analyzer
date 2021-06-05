60078c21687325c8bb61fae3f83b8b75cc1614e3
Mark Ablovatskii <mark.ablov@gmail.com>
2021-06-05 23:03:21 +0700

    extend script with functionality to retrieve package.json from each repo for further analysis

--- src/scripts/analyzeUsageOfApiClient.js
+++ src/scripts/analyzeUsageOfApiClient.js
@@ -2,13 +2,58 @@
 
 require('dotenv-defaults/config');
 
-const ORGANIZATION = 'motorway';
+const fs = require('fs');
+const path = require('path');
 
-const { listOrgRepos } = require('../services/githubService.js');
+const { listOrgRepos, getFileContent } = require('../services/githubService.js');
+const { runInParallel } = require('../workers/promisePool.js');
+
+const ORGANIZATION = 'motorway';
+const TASKS_CONCURRENCY = 5;
+
+const temporaryFolder = path.resolve(__dirname, 'tempFiles');
+
+/*
+ * Get content of package.json
+ */
+const getPackageJsonForRepo = async (repo) => {
+  try {
+    const { content: packageBase64 } = await getFileContent({ organization: ORGANIZATION, repo, path: 'package.json' });
+    const packageString = Buffer.from(packageBase64, 'base64').toString('utf8');
+    return JSON.parse(packageString);
+  } catch (err) {
+    return null;
+  }
+};
+
+/*
+ * Main worker for repo analysis
+ */
+const analyzeRepoSource = async (repo) => {
+  const packageJson = await getPackageJsonForRepo(repo);
+  if (!packageJson) {
+    console.log(`[-] skipping ${repo}: no package.json`);
+  }
+};
 
 const main = async () => {
   const repos = await listOrgRepos({ organization: ORGANIZATION });
   console.log(`[+] Found ${repos.length} repositories for organization.`);
+
+  if (!fs.existsSync(temporaryFolder)) {
+    fs.mkdirSync(temporaryFolder);
+  }
+
+  const appList = repos
+    .filter(({ private: isPrivate, archived, disabled }) => isPrivate && !archived && !disabled)
+    .map(({ name }) => name);
+  console.log(`[+] ${appList.length} repositories are active.`);
+
+  await runInParallel(
+    TASKS_CONCURRENCY,
+    appList.map((appName) => ({ id: appName })),
+    analyzeRepoSource,
+  );
 };
 
 main().catch((err) => console.error(err));
